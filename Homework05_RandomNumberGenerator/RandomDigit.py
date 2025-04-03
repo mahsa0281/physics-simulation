791ad0b09cde8f38071d41ed69923d42b54d87ba
mahsa0281 <rh.mahsa2oo2@gmail.com>
2025-04-03 21:38:48 +0330

    Update Homework05_RandomNumberGenerator/RandomDigit.py with new logic

--- Homework05_RandomNumberGenerator/RandomDigit.py
+++ Homework05_RandomNumberGenerator/RandomDigit.py
@@ -2,13 +2,14 @@ import numpy as numpy
 import random
 import matplotlib.pyplot as plt
 
-N = 10000  
+N = 100 
 results = [random.randint(0, 9) for _ in range(N)]
 
 frequencies = [results.count(i) for i in range(10)]
 
+title_str = f'Frequency of Each Digit (0-9) for N = {N}'
 plt.bar(range(10), frequencies, tick_label=range(10))
 plt.xlabel('Digit')
 plt.ylabel('Frequency')
-plt.title('Frequency of Each Digit (0-9)')
+plt.title(title_str)
 plt.show()
